4ae864f7c071f0df7d9e5d03f55b10832a584402
Dany Robert <danyrt@wahni.com>
2021-12-06 12:21:16 +0530

    fix: remove price

--- bldc/www/shop.py
+++ bldc/www/shop.py
@@ -2,4 +2,3 @@ import frappe
 
 def get_context(context):
     context.data = frappe.get_all('Website Item', filters={'published': 1}, fields=['*'])
-    context.price = frappe.get_all('Item Price', filters={'price_list': 'Customer Selling'}, fields=['*'])
